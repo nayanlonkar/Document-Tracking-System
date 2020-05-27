8f4edacab2fd24838ddea360553c33ece66c43be
Nayan Lonkar <nayanlonkar@gmail.com>
2020-05-27 22:30:17 +0530

    backend - login api

--- backend/api/login.js
+++ backend/api/login.js
@@ -3,7 +3,22 @@ const router = express.Router();
 const connection = require("../db_conf/db_connection");
 
 router.post("/login", (req, res) => {
-  console.log(req.body);
-  res.send("this is login page");
+  const result = async () =>
+    await connection.query(
+      "SELECT * FROM user_details WHERE username=?",
+      [req.body.username],
+      (err, result, fields) => {
+        if (err) {
+          res.status(400).json({ error: "bad request" });
+        }
+        if (result[0] == undefined) {
+          res.status(401).json({ error: "username does not exist" });
+        }
+        res.status(200).json({ message: "success" });
+      }
+    );
+
+  result();
+  // res.status(200).json({ message: "success" });
 });
 module.exports = router;
