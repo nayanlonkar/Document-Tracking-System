5ae4bae398fd4e698885f9d935094d063aec3001
Nayan Lonkar <nayanlonkar@gmail.com>
2020-06-01 19:37:35 +0530

    backend - updated schema

--- backend/db_conf/schema.sql
+++ backend/db_conf/schema.sql
@@ -13,6 +13,17 @@ CREATE TABLE IF NOT EXISTS files (
     id              INT         AUTO_INCREMENT PRIMARY KEY,
     file_name       CHAR(25)    NOT NULL,
     doc_type        CHAR(25)    NOT NULL,
-    file_owner      CHAR(25)    NOT NULL,
-    FOREIGN KEY (file_owner) REFERENCES user_details(username)
+    user_id         INT    NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user_details(id)
+) ENGINE = INNODB CHARACTER SET=utf8;
+
+CREATE TABLE IF NOT EXISTS userFiles (
+    id              INT         AUTO_INCREMENT  PRIMARY KEY,
+    file_id         INT         NOT NULL,
+    sender          INT         NOT NULL, 
+    receier         INT         NOT NULL,
+    is_received     INT         DEFAULT 0,
+    FOREIGN KEY (file_id) REFERENCES files(id),
+    FOREIGN KEY (sender) REFERENCES user_details(id),
+    FOREIGN KEY (receier) REFERENCES user_details(id)
 ) ENGINE = INNODB CHARACTER SET=utf8;
